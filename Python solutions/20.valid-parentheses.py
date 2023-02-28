a4bf329944549aa2ed8baba7a3fe891c9fd97b9d
supersanta183 <klemmensen11@gmail.com>
2023-02-28 16:11:36 +0100

    valid parenthesis

--- Python solutions/20.valid-parentheses.py
+++ Python solutions/20.valid-parentheses.py
@@ -1,4 +1,12 @@
-def isValid( s):
+#
+# @lc app=leetcode id=20 lang=python
+#
+# [20] Valid Parentheses
+#
+
+# @lc code=start
+class Solution(object):
+    def isValid(self, s):
         """
         :type s: str
         :rtype: bool
@@ -34,4 +42,5 @@ def isValid( s):
             return True
         return False
 
-print(isValid("("))
+# @lc code=end
+
