1626d68017bd8ba7f38edced9210006b1cda2d96
supersanta183 <klemmensen11@gmail.com>
2023-03-13 02:46:31 +0100

    task solved

---
+++ Python solutions/35.search-insert-position.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=35 lang=python
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        low = 0
+        high = len(nums) - 1
+        mid = 0
+        
+        while low <= high:
+            mid = (low+high)//2
+            if nums[mid] > target:
+                high = mid - 1
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                return mid
+        return low
+# @lc code=end
+
